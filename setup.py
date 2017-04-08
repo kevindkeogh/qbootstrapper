41b47e482e5f193f2306a63773ef8a28d0a523d4
Kevin <kevin.d.keogh@gmail.com>
2017-04-08 17:20:50 -0400

    rearrange directory

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+VERSION = '0.1'
+
+setup(
+        name='qbootstrapper',
+        version=VERSION,
+        packages='qboostrapper',
+        license='MIT',
+     )
